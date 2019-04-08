32094f35888663f3416430f0b398868c5fa2b9d0
Yehuda Gilead <yehuda@chegg.com>
2019-04-08 18:28:12 +0300

    — extensible summary

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react'
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 export default class    Summary extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            is_expanded : false
+        }
+    }
     render() {
         return (
-            <section className='Summary Content'>
+            <section className={`Summary Content ${this.state.is_expanded ? `expanded` : ``}`}>
+                <span className='toggle'
+                      onClick={() => {this.toggleView()}}>
+                    {this.state.is_expanded ? `לקפל` : `להרחיב`}
+                </span>
                 <h3 className='title'>סיכום השיעור</h3>
                 <div className='content'>
                     {documentToReactComponents(this.props.content)}
                 </div>
             </section>
         )
+    }
+
+    toggleView() {
+        this.setState({ is_expanded : !this.state.is_expanded });
     }
 }

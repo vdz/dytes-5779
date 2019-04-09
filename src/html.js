03926ec16065fc21e878643cf24a751d48ca5ec6
Yehuda Gilead <yehuda@chegg.com>
2019-04-09 07:09:50 +0300

    - google analytics added

--- src/html.js
+++ src/html.js
@@ -5,6 +5,15 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-40219407-2"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-40219407-2');
+        </script>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
